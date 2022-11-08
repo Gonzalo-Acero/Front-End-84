6a046d602f9434ece135a3819f3268acebbd021e
Gonzalo <gonzalo.acero@davinci.edu.ar>
2022-11-08 13:12:41 -0300

    TSC

--- app/app.ts
+++ app/app.ts
@@ -1,2 +1,46 @@
 let message: string = 'Hello World';
 console.log(message);
+
+
+let a:number = 23; //inferencia explícita
+let b:string = "AOE3"; //inferencia explícita
+let c = 101; // inferencia implícita
+
+//c="one"; string no asignable al tipo number
+
+//let recursos:['memoria', 'disco', 'procesador'];  array inferido implicitamente
+
+let recursos:string [] = ['memoria', 'disco', 'procesador'] //array inferido explicitamente
+
+
+
+function calcularIva (productos:Producto[]) :[number, number]{
+
+    let total = 0;
+    productos.forEach(({precio})=>{
+        total += precio;
+    });
+
+    return [total, total*0.15];
+
+}
+
+
+//Clase Producto
+class Producto {
+    precio:number;
+    codigobarras:string;
+
+
+    constructor(precio:number, codigobarras:string){
+        this.precio = precio;
+        this.codigobarras = codigobarras;
+    }
+}
+
+
+/*var obj = {a:2, b:3, c:4};
+console.log(obj.c)*/
+
+/*var array =  [1 ,2, 3];
+console.log(array[2]);*/
